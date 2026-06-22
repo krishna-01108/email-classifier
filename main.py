9c60d6a132e699678f92992b792533b04249017f
unknown <krishna.yadav.5963@gmail.com>
2026-06-22 18:23:09 +0530

    CSS completed

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import pickle
 import pandas as pd
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 async def get_output(email_body):
